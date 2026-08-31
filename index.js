fe8c21d3a0d3cc0125811ba362b06b2ede550347
Abaraajith Thirumoorthy <abaraajith.t@gmail.com>
2026-09-01 00:05:30 +0530

    Watchlist Base Functionalities

--- index.js
+++ index.js
@@ -2,41 +2,53 @@
 const searchForm = document.querySelector(".searchForm")// Instead of just the button ,capture the whole form
 const searchField = document.getElementById("searchField")
 const feedSection = document.getElementById("feed")
+const watchListFeedSection = document.getElementById("watchlist-feed")
 let movieResults = []
+const watchList = [] 
 
-
+/* MOVIES RETRIEVAL */
 async function retrieveMovies(searchText){
-
 
       let cleanStr = searchText.replace('"','')
       const res= await fetch(`http://www.omdbapi.com/?s=${cleanStr}&apikey=51a01f27`)
       const data = await res.json()
       console.log(data)
       movieResults=await data.Search
+      let searchResponse= await data.Response
       
       let finalMovieResults = []
-
       let id=0
-      for(let movie of movieResults){
-         id = movie.imdbID.replace('"','')
-         const res2= await fetch(`http://www.omdbapi.com/?i=${movie.imdbID}&apikey=51a01f27`)
-         const data2 = await res2.json()
-         
-         finalMovieResults.push(data2)
-      }
+      if(searchResponse==="False"){
+         feedSection.innerHTML=" "
+         feedSection.innerHTML=`
+         <div class="init-state">
+            <h1 style="text-align:center;">Unable to find what you're looking for.<br>Please try another search.</h1>
+         </div>`
+      }else{
+            for(let movie of movieResults){
+               id = movie.imdbID.replace('"','')
+               const res2= await fetch(`http://www.omdbapi.com/?i=${movie.imdbID}&apikey=51a01f27`)
+               const data2 = await res2.json()
+               
+               finalMovieResults.push(data2)
+               }
 
-      console.log("New movie results:", finalMovieResults)
+            console.log("New movie results:", finalMovieResults)
 
-      feedGenerator(finalMovieResults)
-
+            feedGenerator(finalMovieResults)
+         }
 
    }
 
+/*SUBMIT BUTTON FUNCTION */
 searchForm.addEventListener('submit',(e)=>{
    e.preventDefault();
    let text = searchField.value
    retrieveMovies(text)
 })
+
+
+/* FEED GENERATOR FUNCTION */
 
 function feedGenerator(movies) {
    feedSection.innerHTML=" "
@@ -48,7 +60,6 @@ function feedGenerator(movies) {
                 <h1>Start Exploring</h1>
          </div>`
    }else{
-      
       movies.forEach(function(movie) {
          feedSection.innerHTML+=`
             <div class="movie_card">
@@ -62,19 +73,76 @@ function feedGenerator(movies) {
                         </div>
                     </div>
                     <div class="movie-details-line">
-                        <p class="duration">${movie.Runtime} mins</p>
+                        <p class="duration">${movie.Runtime}</p>
                         <p class="genres">${movie.Genre}</p>
-                        <div class="watchlist-func">
-                            <img src="images/watchlist-add.png">
-                            <p>Watchlists</p>
-                        </div>
+                        <button class="watchlist-btn-add" data-id="${movie.imdbID}">
+                           <img src="images/watchlist-add.png" alt="Add">
+                           <span>Watchlist</span>
+                        </button>
                     </div>
                     <p class="movie-plot">${movie.Plot}</p>
                 </div>
             </div>`
-   })
+      }
+   )
       
    }
 }
 
-//feedGenerator()
+/* WATCHLIST FEED GENERATOR */
+
+function watchListGenerator(movies){
+   watchListFeedSection.innerHTML=" "
+   
+   if(movies.length === 0){
+      watchListFeedSection.innerHTML=`
+         <div class="empty-state">
+                <img id="empty-state-img" src="/images/feed-icon.png">
+                <h1>Start Exploring</h1>
+         </div>`
+   }else{
+      movies.forEach(function(movie) {
+         watchListFeedSection.innerHTML+=`
+            <div class="movie_card">
+                <div class="movie-poster"><img src=${movie.Poster}></div>
+                <div class="movie-description">
+                    <div class="title-rating">
+                        <h2 style="font-weight: 600;">${movie.Title}</h2>
+                        <div class="rating">
+                            <img src="images/star.png">
+                            <p>${movie.imdbRating}</p>
+                        </div>
+                    </div>
+                    <div class="movie-details-line">
+                        <p class="duration">${movie.Runtime}</p>
+                        <p class="genres">${movie.Genre}</p>
+                        <button class="watchlist-btn-remove" data-id="${movie.imdbID}">
+                           <img src="images/watchlist-remove.png" alt="remove">
+                           <span>Watchlist</span>
+                        </button>
+                    </div>
+                    <p class="movie-plot">${movie.Plot}</p>
+                </div>
+            </div>`
+      }
+   )
+      
+   }
+}
+
+feedSection.addEventListener('click', async (e)=>{
+   if(e.target.closest('.watchlist-btn-add')){
+      let id = e.target.closest('.watchlist-btn-add').dataset.id;
+      console.log(id)
+
+      const res2= await fetch(`http://www.omdbapi.com/?i=${id}&apikey=51a01f27`)
+      const data2 = await res2.json()
+               
+      watchList.push(data2)
+      console.log(watchList)
+   }
+})
+
+function addToWatchlist(){
+   console.log("Add to watchlist clicked")
+}
